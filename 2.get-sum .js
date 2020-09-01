aa25f4a71a1a77dd27c99e003dd0106dc70d3bff
itacamba <daniela.sahagun03@gmail.com>
2020-09-01 14:37:45 -0400

    solved get sum of two numbers in a range

---
+++ 2.get-sum .js
@@ -0,0 +1,14 @@
+let alg2 = document.getElementById('2')
+alg2.addEventListener("click", () => {
+    getSum(-1,2)
+})
+
+function getSum(a,b){
+    if(a === b) return a;
+    let orderedNums = [a,b].sort((a,b)=> a-b)
+    let sum = 0
+    for(let i = orderedNums[0]; i <= orderedNums[orderedNums.length - 1]; i++){
+        sum = sum + i
+    }
+    return sum
+}
